40070b0c7c17384e7e9eaac523631d8d943670a8
Ram Kripa <ramkripa@Rams-Air.attlocal.net>
2020-05-22 21:04:31 -0700

    blueberry commit

--- vendor/bundle/ruby/2.6.0/gems/nokogiri-1.10.9/ext/nokogiri/tmp/x86_64-apple-darwin19/ports/libxml2/2.9.10/libxml2-2.9.10/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/Users/ramkripa/Desktop/WEBSITE/rammkripa.github.io/vendor/bundle/ruby/2.6.0/gems/nokogiri-1.10.9/ports/x86_64-apple-darwin19/libxml2/2.9.10/lib"
-XML2_LIBS="-lxml2 -lz  -lpthread  -liconv  -lm "
-XML2_INCLUDEDIR="-I/Users/ramkripa/Desktop/WEBSITE/rammkripa.github.io/vendor/bundle/ruby/2.6.0/gems/nokogiri-1.10.9/ports/x86_64-apple-darwin19/libxml2/2.9.10/include/libxml2"
-MODULE_VERSION="xml2-2.9.10"
-
